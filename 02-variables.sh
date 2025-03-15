c075ac5c44a48c3b25664bd12a830c49e69bddb6
Mickykamera <malathi.kamera@gmail.com>
2025-03-15 15:11:51 +0530

    changes2 done

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Ram
+PERSON2=Raheem
 
 echo "$PERSON1: hellow $PERSON2, goodmornign"
-echo "$PERSON2: hi Ram$PERSON1, very good mornong"
+echo "$PERSON2: hi $PERSON1, very good mornong"
 echo "$PERSON1: How are you $PERSON2?"
 echo "$PERSON2: I am good $PERSON1, How are you?"
