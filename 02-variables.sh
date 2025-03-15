d78b762145bec275286e45589b69fe2cb21ddac1
Mickykamera <malathi.kamera@gmail.com>
2025-03-15 14:49:18 +0530

    cahnges done

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: hellow Suresh, goodmornign"
+echo "Suresh: hi Ramesh, very good mornong"
+echo "Ramesh: How are you Suresh?"
+echo "Suresh: I am good Ramesh, How are you?"
